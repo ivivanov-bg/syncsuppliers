cece8acf008c32243a576c234da59db995d6c028
Ivaylo Ivanov <ivaylo.ivanov@musala.com>
2021-06-09 23:26:31 +0300

    1. Automatically upload pictures for product combinations 2. Initial version of product insertion for Moni toys

--- syncsuppliers/controllers/admin/AdminSyncSuppliers.php
+++ syncsuppliers/controllers/admin/AdminSyncSuppliers.php
@@ -39,7 +39,7 @@ class AdminSyncSuppliersController extends ModuleAdminController {
 	{
 		$products = Supplier::getProductsForSync(1, $this->id_lang, 1000000, 1);
 		
-		$logger = new FileLoggerCore(0);
+		$logger = new FileLogger(0);
 		$logger->setFilename(_PS_ROOT_DIR_.'/log/product_debug.log');
 		
 		
@@ -182,11 +182,11 @@ class AdminSyncSuppliersController extends ModuleAdminController {
 		$xml_data = $this->prepareXmlObject($xml_data_str, $logger);
 		
 		if ($xml_data != false) {
-		    $xml_products = $xml_data->xpath($xpath_products);
-		    $xml_products_cnt = count($xml_products);
+			$xml_products = $xml_data->xpath($xpath_products);
+			$xml_products_cnt = count($xml_products);
 		} else {
-		    $xml_products = null;
-		    $xml_products_cnt = 0;
+			$xml_products = null;
+			$xml_products_cnt = 0;
 		}
 		
 		if ($xml_products_cnt == 0) {
@@ -201,7 +201,7 @@ class AdminSyncSuppliersController extends ModuleAdminController {
 		
 		$logger->logInfo('Products to sync: '. $xml_products_cnt . ' for selector "' . $xpath_products . '".' . ' After preprocessing: ' . $sync_products_cnt);
 		
-		$products = SupplierCore::getProductsForSync($id_supplier, $this->id_lang, 1000000, 1);
+		$products = Supplier::getProductsForSync($id_supplier, $this->id_lang, 1000000, 1);
 		$products_cnt = count($products);
 		
 		$logger->logInfo("Products in database: ". $products_cnt . " for supplier " . $id_supplier . " with 'supplier_reference'");
@@ -276,7 +276,11 @@ class AdminSyncSuppliersController extends ModuleAdminController {
 					}
 					
 					if (($current_product == null && $sync_prd_fields['parent_pid'] == null) && !$sync_prd_fields['sync_prd_insert_allowed']) {
-						$logger->logDebug('Skipping ' . $current_key . ' of ' . $xml_products_cnt . ($sync_prd_fields['parent_pid'] != null ? ': (NEW ATTR) ' : ': (NEW) ') . $sync_prd_fields['sync_prd_ref'] . ' ' . $sync_prd_fields['sync_prd_name_bg']);
+						$logger->logDebug(
+							'Skipping ' . $current_key . ' of ' . $xml_products_cnt . ($sync_prd_fields['parent_pid'] != null ? ': (NEW ATTR) ' : ': (NEW) ') 
+							. $sync_prd_fields['sync_prd_ref'] . ' ' . $sync_prd_fields['sync_prd_name_bg'] 
+							. ' (Price ' . $sync_prd_fields['sync_prd_price'] . ', Quantity ' . $sync_prd_fields['sync_prd_quantity'] . ')'
+						);
 						continue;
 					}
 					
@@ -326,7 +330,7 @@ class AdminSyncSuppliersController extends ModuleAdminController {
 					$D++;
 				} else {
 					$logger->logInfo('Processed ' . (++$i) . ' of ' . $missing_products_cnt . ': [K] ('. $prd_id . ') '
-						. $current_product['supplier_ref'] . ' ' . $current_product['name']);
+						. $current_product['supplier_ref'] . ' ' . $current_product['name'] . ' (Quantity ' . $prd_quantity . ')');
 					$K++;
 				}
 			}
@@ -356,16 +360,16 @@ class AdminSyncSuppliersController extends ModuleAdminController {
 		if (!$xml_data) {
 			$logger->logError('Failed loading file "'. $xml_data_str . '"');
 		} else {
-    		$logger->logDebug('Found '. count($xml_data) . ' Nodes in XML.');
-    		
-    		foreach ($xml_data->getNamespaces(true) as $key => $value) {
-    			if (!$key) {
-    				$key = 'df';
-    			}
-    			
-    			$logger->logInfo('Found XML schema: "'. $key . '=' . $value .'"');
-    			$xml_data->registerXPathNamespace($key, $value);
-    		}
+			$logger->logDebug('Found '. count($xml_data) . ' Nodes in XML.');
+			
+			foreach ($xml_data->getNamespaces(true) as $key => $value) {
+				if (!$key) {
+					$key = 'df';
+				}
+				
+				$logger->logInfo('Found XML schema: "'. $key . '=' . $value .'"');
+				$xml_data->registerXPathNamespace($key, $value);
+			}
 		}
 		
 		return $xml_data;
@@ -386,6 +390,7 @@ class AdminSyncSuppliersController extends ModuleAdminController {
 		}
 		
 		$prd_id = 0;
+		$p_attr = 0;
 		
 		if ($current_product != null) {
 			$prd_id = $current_product['id_product'];
@@ -412,13 +417,17 @@ class AdminSyncSuppliersController extends ModuleAdminController {
 			$prd_quantity = (int) StockAvailable::getQuantityAvailableByProduct($p->id, $p_attr);
 			
 			if ($p_attr_color_expected != $p_attr_color) {
-				$logStatus .= 'Expected attribute: ' . $p_attr_color_expected . ', but actual is: ' . $p_attr_color . ', ';
-				
+				if ($current_product != null) {
+					$logStatus .= 'Expected attribute: ' . $p_attr_color_expected . ', but actual is: ' . $p_attr_color . ', ';
+				} else {
+					$logStatus .= 'NEW attribute: ' . $p_attr_color_expected . ', ';
+				}
 				$combinationAttributes[] = $p_attr_color_expected;
 				
 				if(!$p->productAttributeExists($combinationAttributes)) {
 					$id_product_attribute = $p->addCombinationEntity(0, 0, 0, '', 0, 0, null, '', $id_supplier, '', (isset($p_attr_color) && $p_attr_color == 0));
 					$combination = new Combination((int)$id_product_attribute);
+					$combination->id_product = $prd_id;
 					
 					$combination->setAttributes($combinationAttributes);
 					
@@ -431,19 +440,24 @@ class AdminSyncSuppliersController extends ModuleAdminController {
 					$p->save();
 					
 					$p_attr = $id_product_attribute;
+					$prd_price = -1;
+					$prd_quantity = -1;
 				}
 			}
 			
+			// Check price
 			if ($prd_price != $sync_prd_fields['sync_prd_price']) {
 				$logStatus .= 'Price ' . $prd_price . ' -> ' . $sync_prd_fields['sync_prd_price'] . ', ';
 				$p->price = $sync_prd_fields['sync_prd_price'];
 				$isUpdated = true;
 			}
 
+			// Check Quantity
 			if($prd_quantity != $sync_prd_fields['sync_prd_quantity']) {				
 				$logStatus .= 'Quantity ' . $prd_quantity . ' -> ' . $sync_prd_fields['sync_prd_quantity'] . ', ';
 				StockAvailable::setQuantity($p->id, $p_attr, $sync_prd_fields['sync_prd_quantity']);
 				
+				// if the quantity is > 0 - set this attribute as default to prevent showing attributes out of stock
 				if (isset($p_attr) && $p_attr > 0 && $sync_prd_fields['sync_prd_quantity'] > 0) {
 					$p->deleteDefaultAttributes();
 					$p->setDefaultAttribute($p_attr);
@@ -452,10 +466,58 @@ class AdminSyncSuppliersController extends ModuleAdminController {
 				$isUpdated = true;
 			}
 			
+			// check if there is an image for this product combination and upload if not
+			if (isset($p_attr) && $p_attr > 0 && !Image::hasImages($this->id_lang, $prd_id, $p_attr)) {
+				
+				$logStatus .= 'Add images: ';
+				
+				$image_ids = array();
+				
+				$img_key = 0;
+				foreach ($sync_prd_fields['sync_prd_image_links'] as $image_link) {
+					$url = (string) $image_link;
+					$img_key++;
+					
+					if (substr( $url, 0, 4 ) !== "http") {
+						$url = 'http://'.$url;
+					}
+					
+					$image = new Image();
+					$image->legend = $sync_prd_fields['sync_prd_name_bg'];
+					$image->id_product = $prd_id;
+					$image->position = Image::getHighestPosition($prd_id) + 1;
+					$image->cover = false;
+					
+					if (($image->validateFields(false, true)) === true && ($image->validateFieldsLang(false, true)) === true && $image->add()) {
+						$image->associateTo($id_shop);
+						if (!AdminImportController::copyImg($p->id, $image->id, $url, 'products', false)) {
+							$image->delete();
+							$logStatus .= $img_key . ' - failed to copy, ';
+						} else {
+							// SUCCESS -> Add image ID to the array
+							array_push($image_ids, $image->id);
+							$logStatus .= $img_key . ',';
+						}
+					} else {
+						$logStatus .= $img_key . ' - failed to add, ';
+					}
+				}
+				
+				if (!empty($image_ids)) {
+					$combination = new Combination((int)$p_attr);
+					$combination->setImages($image_ids);
+					$combination->save();
+					$isUpdated = true;
+				}
+			} else {
+				$logStatus .= 'Has images, ';
+			}
+			
 			if ($isUpdated) {
 				$result = 'U';
 				$p->save();
 			} else {
+				$logStatus .= 'Price ' . $prd_price . ', Quantity ' . $prd_quantity;
 				$result = 'K';
 			}
 
@@ -466,53 +528,67 @@ class AdminSyncSuppliersController extends ModuleAdminController {
 			$p->supplier_reference  = $sync_prd_fields['sync_prd_ref'];
 			$p->price               = $sync_prd_fields['sync_prd_price'];
 			$p->quantity            = $sync_prd_fields['sync_prd_quantity'];
-			$p->id_category_default = 2;
+			$p->id_category_default = $sync_prd_fields['sync_prd_category'];
 			$p->id_manufacturer     = AdminSyncSuppliersController::getManufacturerIdByName($sync_prd_fields['sync_prd_manufacturer_name'], $logger);
 			
-			$p->name[2]             = $sync_prd_fields['sync_prd_name_bg'];
+			$p->name[2]             = isset($sync_prd_fields['sync_prd_name_bg_insert']) ? $sync_prd_fields['sync_prd_name_bg_insert'] : $sync_prd_fields['sync_prd_name_bg'];
 			$p->link_rewrite[2]     = Tools::str2url($sync_prd_fields['sync_prd_name_bg']);
 			$p->description[2]      = $sync_prd_fields['sync_prd_description_bg'];
 			$p->description[1]      = $sync_prd_fields['sync_prd_description_en'];
 			
 			$p->save();
 			
+			$prd_id = $p->id;
+			
 			$p->updateCategories(array(2));
 			$p->setAdvancedStockManagement(1);
 			
 			if($p_attr_color_expected == 0) {
 				$p->addSupplierReference($id_supplier, null, $sync_prd_fields['sync_prd_ref']);
-				StockAvailable::setQuantity($p->id, null, $sync_prd_fields['sync_prd_quantity']);
+				StockAvailable::setQuantity($prd_id, null, $sync_prd_fields['sync_prd_quantity']);
 			} else {
 				$combinationAttributes[] = $p_attr_color_expected;
 				
 				if(!$p->productAttributeExists($combinationAttributes)) {
-					$id_product_attribute = $p->addCombinationEntity(0, 0, 0, '', 0, 0, null, '', $id_supplier, '', false);
-					$combination = new CombinationCore((int)$id_product_attribute);
+					$id_product_attribute = $p->addCombinationEntity(0, 0, 0, '', 0, 0, null, '', $id_supplier, '', true);
+					$combination = new Combination((int)$id_product_attribute);
+					$combination->id_product = $prd_id;
 					
 					$combination->setAttributes($combinationAttributes);
 					
 					$combination->save();
 					
+					$p->price = $sync_prd_fields['sync_prd_price'];
+					$p->supplier_reference = '';
+					
+					$p->addSupplierReference($id_supplier, 0, '');
 					$p->addSupplierReference($id_supplier, $id_product_attribute, $sync_prd_fields['sync_prd_ref']);
-					StockAvailable::setQuantity($p->id, $id_product_attribute, $sync_prd_fields['sync_prd_quantity']);
+					StockAvailable::setQuantity($prd_id, $id_product_attribute, $sync_prd_fields['sync_prd_quantity']);
+					
+					$p_attr = $id_product_attribute;
 				}
 			}
 			
 			$p->save();
 			
-			
 			$has_cover = false;
+			$image_ids = array();
 			
+			$logStatus .= 'Add images: ';
+			
+			$img_key = 0;
 			foreach ($sync_prd_fields['sync_prd_image_links'] as $image_link) {
 				$url = (string) $image_link;
+				$img_key++;
 				
 				if (substr( $url, 0, 4 ) !== "http") {
 					$url = 'http://'.$url;
 				}
 				
 				$image = new Image();
-				$image->id_product = $p->id;
-				$image->position = Image::getHighestPosition($p->id) + 1;
+				$image->legend = $sync_prd_fields['sync_prd_name_bg'];
+				$image->id_product = $prd_id;
+				$image->position = Image::getHighestPosition($prd_id) + 1;
 				if ($has_cover) {
 					$image->cover = false;
 				} else {
@@ -520,26 +596,38 @@ class AdminSyncSuppliersController extends ModuleAdminController {
 					$image->cover = true;
 				}
 				
-				if (($image->validateFields(false, true)) === true &&
-					($image->validateFieldsLang(false, true)) === true && $image->add())
-				{
+				if (($image->validateFields(false, true)) === true && ($image->validateFieldsLang(false, true)) === true && $image->add()) {
 					$image->associateTo($id_shop);
-					if (!AdminImportController::copyImg($p->id, $image->id, $url, 'products', false))
-					{
+					if (!AdminImportController::copyImg($prd_id, $image->id, $url, 'products', false)) {
 						$image->delete();
+						$logStatus .= $img_key . ' - failed to copy,';
+					} else {
+						// SUCCESS -> Add image ID to the array
+						array_push($image_ids, $image->id);
+						$logStatus .= $img_key . ',';
 					}
+				} else {
+					$logStatus .= $img_key . ' - failed to add,';
 				}
+				
+				$logStatus .= 'processed ' . $img_key . ', ';
+			}
+			if ($p_attr !== 0) {
+				$combination = new Combination((int)$p_attr);
+				$combination->setImages($image_ids);
+				$combination->save();
 			}
 			
 			$p->save();
 			$result = 'I';
+			$logStatus .= ' Price ' . $sync_prd_fields['sync_prd_price'] . ', Quantity ' . $sync_prd_fields['sync_prd_quantity'];
 		}
 		
 		if ($result == 'K') {
-			$logger->logDebug('Processed ' . ($key) . ' of ' . $sync_products_cnt . ': [' . $result . '] ' . ($prd_id != 0 ? '('. $prd_id . ') ' : '(NEW) ')
+			$logger->logDebug('Processed ' . ($key) . ' of ' . $sync_products_cnt . ': [' . $result . '] ' . ($prd_id != 0 ? '('. $prd_id . ',' . $p_attr . ') ' : '(NEW) ')
 				. $sync_prd_fields['sync_prd_ref'] . ' ' . $sync_prd_fields['sync_prd_name_bg'] . ' (' . $logStatus . ')');
 		} else {
-			$logger->logInfo('Processed ' . ($key) . ' of ' . $sync_products_cnt . ': [' . $result . '] ' . ($prd_id != 0 ? '('. $prd_id . ') ' : '(NEW) ')
+			$logger->logInfo('Processed ' . ($key) . ' of ' . $sync_products_cnt . ': [' . $result . '] ' . ($prd_id != 0 ? '('. $prd_id . ',' . $p_attr . ') ' : '(NEW) ')
 				. $sync_prd_fields['sync_prd_ref'] . ' ' . $sync_prd_fields['sync_prd_name_bg'] . ' (' . $logStatus . ')');
 		}
 
@@ -609,7 +697,8 @@ class AdminSyncSuppliersController extends ModuleAdminController {
 
 		$result['sync_prd_ref']               = (string) trim($result['sync_prd_ref']);
 		$result['sync_prd_ref_key']           = (string) 'sr_'.$result['sync_prd_ref'];
-		
+
+		$result['sync_prd_category']          = 23; // Дървени образователни играчки
 		$result['sync_prd_price']             = (double) $sync_prd_xml_node->RRP;
 		$result['sync_prd_quantity']          = (string) $sync_prd_xml_node->Quantity;
 		$result['sync_prd_manufacturer_name'] = (string) $sync_prd_xml_node->Brand;
@@ -687,6 +776,15 @@ class AdminSyncSuppliersController extends ModuleAdminController {
 				$logger->logWarning('Unknown sync_prd_availability "'. $result['sync_prd_availability'] . '"');
 				break;
 		}
+		
+		switch($sync_prd_xml_node->Category->CategoryID) {
+			case 79: // ДЕТСКИ КУХНИ И АКСЕСОАРИ
+			case 48: // КУКЛИ КЪЩИ И МЕБЕЛИ
+			default:
+				$result['sync_prd_category']          = 2;
+				break;
+		}
+		
 		
 		$result['sync_prd_manufacturer_name'] = (string) $sync_prd_xml_node->BrandName->BG;
 		$result['sync_prd_description_bg']    = (string) $sync_prd_xml_node->ProductDetailedDescription;
@@ -783,6 +881,18 @@ class AdminSyncSuppliersController extends ModuleAdminController {
 				break;
 		}
 		
+		switch((string) $sync_prd_xml_node->group) {
+			case '1.КЕНГУРА И КОЛАНИ ЗА ПРОХОЖДАНЕ':
+				$result['sync_prd_category'] = 32; // Проходилки, слайдери и др.
+				break;
+			case '2.ИГРАЧКИ ЗА ПЯСЪК И ТРЕВА':
+				$result['sync_prd_category'] = 34; // Активни игри и съоръжения -> Игра на открито
+				break;
+			default:
+				$result['sync_prd_category'] = 2; // Products 
+				break;
+		}
+		
 		$result['sync_prd_manufacturer_name'] = (string) $sync_prd_xml_node->brand;
 		$result['sync_prd_description_bg']    = (string) $sync_prd_xml_node->description;
 		$result['sync_prd_image_links']       =          $sync_prd_xml_node->image_link;
@@ -829,7 +939,8 @@ class AdminSyncSuppliersController extends ModuleAdminController {
 		//	            $result['sync_prd_description_en']    = (string) ($sync_prd_xml_node->xpath('characteristics/characteristic[@externalCode="231"]')[0]);
 		//	            $result['sync_prd_name_en']           = (string) ($sync_prd_xml_node->xpath('characteristics/characteristic[@externalCode=233]')[0]);
 		
-		$result['sync_prd_insert_allowed']           = false;
+		$allowed_ref_to_insert = array(); // '106351', '107284', '107107'
+		$result['sync_prd_insert_allowed']           = in_array($result['sync_prd_ref'], $allowed_ref_to_insert);
 		
 		//AdminSyncSuppliersController::getManufacturerIdByName($result['sync_prd_manufacturer_name'], $logger);
 		//return false;
@@ -847,21 +958,22 @@ class AdminSyncSuppliersController extends ModuleAdminController {
 		$attr_color[20] = '/ бял/i';
 		$attr_color[27] = '/ деним/i';
 		$attr_color[22] = '/ жълт/i';
-		$attr_color[18] = '/ зелен/i';
+		$attr_color[18] = '/ (зелен|зелена)/i';
 		$attr_color[10] = '/ капучино/i';
 		$attr_color[17] = '/ кафяв/i';
 		$attr_color[8]  = '/ лилав/i';
 		$attr_color[25] = '/ ментов/i';
 		$attr_color[21] = '/ оранжев/i';
-		$attr_color[5]  = '/ (розов|роз)/i';
+		$attr_color[5]  = '/ (розов|роз|розова)/i';
 		$attr_color[24] = '/ светлобежов/i';
 		$attr_color[24] = '/ светлосив/i';
 		$attr_color[28] = '/ светлосин/i';
 		$attr_color[11] = '/ сив/i';
-		$attr_color[6]  = '/ син/i';
+		$attr_color[6]  = '/ (син|синя)/i';
 		$attr_color[23] = '/ тъмносив/i';
+		$attr_color[30] = '/ тъмносин/i';
 		$attr_color[9]  = '/ (тюркоаз|тюрк)/i';
-		$attr_color[7]  = '/ червен/i';
+		$attr_color[7]  = '/ (червен|червена)/i';
 		$attr_color[12] = '/ черен/i';
 		
 		$refined_products = array();
@@ -876,6 +988,7 @@ class AdminSyncSuppliersController extends ModuleAdminController {
 				if (preg_match($av, $sync_prd_attr_key)) {
 					$sync_prd_attr_key = preg_replace($av, ' color_attr',  $sync_prd_attr_key);
 					$sync_prd_fields['attr_color'] = $ak;
+					$sync_prd_fields['sync_prd_name_bg_insert'] = preg_replace($av, '',  $sync_prd_fields['sync_prd_name_bg']);
 					break;
 				}
 			}
